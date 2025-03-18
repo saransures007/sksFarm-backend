c8d7be3ad46e7474169234e0df25eceee64955fe
Saransuresh <saransuresh1999@outlook.com>
2025-03-18 21:18:48 +0530

    test

--- controllers/appControllers/totalMilkProductionController/create.js
+++ controllers/appControllers/totalMilkProductionController/create.js
@@ -1,6 +1,7 @@
 const create = async (Model, req, res) => {
   const { entryDate, totalMilk, avgSnf, avgFat, ratePerLiter, addedBy } = req.body;
 
+  console.log("create", req);
   // Ensure required fields are provided
   if (!entryDate || !totalMilk || !avgSnf || !avgFat || !ratePerLiter || !addedBy) {
     return res.status(400).json({
